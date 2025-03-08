f018f30d5c7e323ed50ed913c7f43cc9a9c39e0f
Johan Montorfano <me@johanmontorfano.com>
2025-03-08 21:26:10 +0100

    add: datalis formatted top bar

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
-export const SITE_TITLE = 'Datalis R&D\'s Blog';
+export const SITE_TITLE = 'Datalis R&D - Blog';
+export const SITE_TITLE_NAV = "Datalis R&D"
 export const SITE_DESCRIPTION = 'Find out everything about our work at Datalis R&D here, and get updates on our released patents and products!';
