06995f4abb1cb22f92f6cb53f69800d0ddf6da26
Johan Montorfano <me@johanmontorfano.com>
2025-03-08 21:03:44 +0100

    update: SITE_* constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Datalis R&D\'s Blog';
+export const SITE_DESCRIPTION = 'Find out everything about our work at Datalis R&D here, and get updates on our released patents and products!';
